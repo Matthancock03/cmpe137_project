21cbcf4e916850562d5eb95a61bf36428609d809
ans0nJ <ans0nJ72@gmail.com>
2015-11-17 18:47:10 -0800

    added inputs and outputs added reset button

--- Mortgage_Calculator/app/src/main/java/com/example/misanthropic/mortgage_calculator/MainActivity.java
+++ Mortgage_Calculator/app/src/main/java/com/example/misanthropic/mortgage_calculator/MainActivity.java
@@ -1,16 +1,107 @@
 package com.example.misanthropic.mortgage_calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.lang.Math;
+import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
+    EditText editText5;
+
+    TextView answer1;
+    TextView answer2;
+    TextView answer3;
+    TextView answer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ResetButton = (Button) findViewById(R.id.Reset);
+        Button Calculate = (Button) findViewById(R.id.Calculate);
+
+        editText1 = (EditText) findViewById(R.id.HomeValueField);
+        editText2 = (EditText) findViewById(R.id.DownPayField);
+        editText3 = (EditText) findViewById(R.id.APRField);
+        editText4 = (EditText) findViewById(R.id.TermsField);
+        editText5 = (EditText) findViewById(R.id.TaxRateField);
+
+        answer1 = (TextView) findViewById(R.id.Answer1);
+        answer2 = (TextView) findViewById(R.id.Answer2);
+        answer3 = (TextView) findViewById(R.id.Answer3);
+        answer4 = (TextView) findViewById(R.id.Answer4);
+
+        ResetButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        editText1.setText("");
+                        editText2.setText("");
+                        editText3.setText("");
+                        editText4.setText("");
+                        editText5.setText("");
+                    }
+                }
+        );
+
+        Calculate.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        double Homevalue = 0;
+                            try {
+                                Homevalue = Double.parseDouble(editText1.getText().toString().replaceAll("[.,]", ""));
+                            }
+                            catch (NumberFormatException e){
+
+                            }
+                        double DownPay = 0;
+                            try{
+                                DownPay = Double.parseDouble(editText2.getText().toString().replaceAll("[.,]",""));
+                            }
+                            catch (NumberFormatException e){
+
+                            }
+                        double APR = 0;
+                            try {
+                                APR = Double.parseDouble(editText3.getText().toString());
+                            }
+                            catch (NumberFormatException e){
+
+                            }
+                        double Terms = 0;
+                            try {
+                                Terms = Double.parseDouble(editText4.getText().toString());
+                            }
+                            catch (NumberFormatException e){
+
+                            }
+                        double TaxRate = 0;
+                            try {
+                                Double.parseDouble(editText5.getText().toString());
+                            }
+                            catch (NumberFormatException e){
+
+                            }
+
+                        double MonthlyPayment = DownPay*((APR*Math.pow((1+APR), Terms))/(Math.pow((1+APR), Terms)-1));
+
+                        DecimalFormat format = new DecimalFormat("#,###.00");
+                        answer3.setText(format.format(MonthlyPayment));
+                    }
+                }
+        );
+
     }
 
     @Override
