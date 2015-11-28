25f520f0c5352eb7c998bf76c0693e0a56fbf43e
Matt Hancock <matthancock03@gmail.com>
2015-11-27 20:04:59 -0800

    added more fragments

--- PicMe/app/src/main/java/com/example/misanthropic/picme/CreateAlbumFragment.java
+++ PicMe/app/src/main/java/com/example/misanthropic/picme/CreateAlbumFragment.java
@@ -2,16 +2,21 @@ package com.example.misanthropic.picme;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.firebase.client.Firebase;
+
+import java.io.IOException;
 
 
 /**
@@ -29,6 +34,7 @@ public class CreateAlbumFragment extends Fragment {
     MainActivity main = (MainActivity)getActivity();
     String email = main.getUserEmail();
     EditText albumName;
+    private int PICK_IMAGE_REQUEST = 1;
 
     private OnFragmentInteractionListener mListener;
 
@@ -83,12 +89,37 @@ public class CreateAlbumFragment extends Fragment {
         mListener = null;
     }
 
+    public void uploadImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
     public void createAlbum(View v){
         albumName = (EditText) v.findViewById(R.id.albumName);
-
         Log.d("Debug", albumName.toString());
+
     }
-    
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == main.RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(main.getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                //ImageView imageView = (ImageView) main.findViewById(R.id.imageView);
+                //imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
