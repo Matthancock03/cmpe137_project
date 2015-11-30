acc446129bb90a0eb4f9f2b71a0f5ed8b4b8407f
ans0nJ <ans0nJ72@gmail.com>
2015-11-29 23:40:27 -0800

    Added add/delete buttons

--- PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumViewFragment.java
+++ PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumViewFragment.java
@@ -1,6 +1,8 @@
 package com.example.misanthropic.picme;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -10,7 +12,13 @@ import android.view.ViewGroup;
 
 import com.firebase.client.Firebase;
 
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.BaseAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +34,8 @@ public class AlbumViewFragment extends Fragment {
     Firebase user;
     MainActivity main = (MainActivity)getActivity();
     String email = main.getUserEmail();
+
+    private int PICK_IMAGE_REQUEST = 1;
 
     private OnFragmentInteractionListener mListener;
 
@@ -55,17 +65,53 @@ public class AlbumViewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_album_view, container, false);
 
         GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        Button addphoto = (Button) view.findViewById(R.id.addphoto);
+        Button deletephoto = (Button) view.findViewById(R.id.deletephoto);
+
+        // TODO: get images from Firebase
+
+        //gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(HelloGridView.this, "" + position,
+                //      Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        addphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: implement add image
+            }
+        });
+
+        deletephoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
 
     }
 
+    public void uploadImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    /*
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
+    }*/
 
     @Override
     public void onAttach(Activity activity) {
