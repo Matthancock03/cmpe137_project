801bc98870942e346d85a4b82a1ffd47439bac67
ans0nJ <ans0nJ72@gmail.com>
2015-12-02 22:11:55 -0800

    Added dummy pics for the album view

--- PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumView.java
+++ PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumView.java
@@ -1,13 +1,24 @@
 package com.example.misanthropic.picme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class AlbumView extends AppCompatActivity {
+public class AlbumView extends FragmentActivity {
+
+    private Button createAlbum;
+    private Button deleteAlbum;
+
+    final Integer [] myThumbs = {
+            R.mipmap.sample_2
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +27,12 @@ public class AlbumView extends AppCompatActivity {
 
         getIntent();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        createAlbum = (Button) findViewById(R.id.AddAlbum);
+        deleteAlbum = (Button) findViewById(R.id.DeleteAlbum);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +42,28 @@ public class AlbumView extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        createAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoCreateAlbum();
+            }
+        });
+
+        gridview.setAdapter(new ImageAdapter(this, myThumbs));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(HelloGridView.this, "" + position,
+                //      Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void gotoCreateAlbum(){
+        Intent GotoCreateAlbum = new Intent(this, CreateAlbum.class);
+        startActivity(GotoCreateAlbum);
     }
 
 }
--- PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumViewFragment.java
+++ PicMe/app/src/main/java/com/example/misanthropic/picme/AlbumViewFragment.java
@@ -1,21 +1,11 @@
 package com.example.misanthropic.picme;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
-import android.widget.GridView;
+import android.support.v4.app.FragmentActivity;
 
 import com.firebase.client.Firebase;
 
-/**
+/*
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link AlbumViewFragment.OnFragmentInteractionListener} interface
@@ -23,17 +13,18 @@ import com.firebase.client.Firebase;
  * Use the {@link AlbumViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AlbumViewFragment extends Fragment {
+public class AlbumViewFragment extends FragmentActivity {
 
     Firebase ref;
     Firebase user;
-    MainActivity main = (MainActivity)getActivity();
+    //MainActivity main = (MainActivity)getActivity();
     //String email = main.getUserEmail();
 
     private int PICK_IMAGE_REQUEST = 1;
 
-    private OnFragmentInteractionListener mListener;
+    //private OnFragmentInteractionListener mListener;
 
+    /*
     public static AlbumViewFragment newInstance(String param1, String param2) {
         AlbumViewFragment fragment = new AlbumViewFragment();
         Bundle args = new Bundle();
@@ -43,51 +34,28 @@ public class AlbumViewFragment extends Fragment {
 
     public AlbumViewFragment() {
         // Required empty public constructor
-    }
+    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Firebase.setAndroidContext(getActivity());
+        //Firebase.setAndroidContext(getActivity());
         ref = new Firebase("https://PicMe.firebaseio.com/users");
         user = ref.child("users").child("email");
     }
 
+    /*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_album_view, container, false);
 
-        GridView gridview = (GridView) view.findViewById(R.id.gridview);
-        Button addAlbum = (Button) view.findViewById(R.id.addalbum);
-        Button deleteAlbum = (Button) view.findViewById(R.id.deletealbum);
+
 
         // TODO: get images from Firebase
 
-        //gridview.setAdapter(new ImageAdapter(this));
 
-        gridview.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                //Toast.makeText(HelloGridView.this, "" + position,
-                //      Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        addAlbum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO: implement add image
-            }
-        });
-
-        deleteAlbum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         return view;
 
@@ -108,6 +76,7 @@ public class AlbumViewFragment extends Fragment {
         }
     }*/
 
+    /*
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -135,9 +104,10 @@ public class AlbumViewFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
+    /*
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
-    }
+    }*/
 
 }
